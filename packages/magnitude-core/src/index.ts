88ae36ce7b461643b3ac304e6a5ce2a41a31ef7f
Anders Lie <anders.erik.lie@gmail.com>
2025-04-22 17:05:32 -0400

    disable BAML terminal logs

--- packages/magnitude-core/src/index.ts
+++ packages/magnitude-core/src/index.ts
@@ -1,4 +1,5 @@
 import { TestCaseAgent } from "@/agent";
+import { setLogLevel } from '@/ai/baml_client/config';
 
 export { TestCaseAgent };
 export * from "@/types";
@@ -6,3 +7,5 @@ export * from "@/web/types";
 export * from "@/recipe/types";
 export * from '@/common';
 export { logger } from './logger';
+
+setLogLevel('OFF');
