cc6f868da27325a12f457aebfe07bde9b1ac6b46
Anders Lie <anders.erik.lie@gmail.com>
2025-04-22 23:02:03 -0400

    cleanup examples

---
+++ packages/magnitude-test/examples/tests/example.mag.ts
@@ -0,0 +1,17 @@
+import { test } from 'magnitude-test';
+
+// Learn more about building test case:
+// https://docs.magnitude.run/core-concepts/building-test-cases
+
+const sampleTodos = [
+    "Take out the trash",
+    "Buy groceries",
+    "Build more test cases with Magnitude"
+];
+
+test('can add and complete todos')
+    .step('create 3 todos')
+        .data(sampleTodos.join(", "))
+        .check('should see all 3 todos')
+    .step('mark each todo complete')
+        .check('says no items left')
